d8f8406839baf9d1d3f8ae373860a1626e013afa
Avi Singhal <avisinghal6@gmail.com>
2022-08-28 13:08:48 -0500

    added loader, loaded posts using api

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPosts } from '../api';
 import Home from '../pages/Home';
+import Loader from './Loader';
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getPosts();
       console.log('response', response);
+
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+      setLoading(false);
     };
     fetchPosts();
   }, []);
 
+  if (loading) return <Loader />;
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
+        <Home posts={posts} />
       </header>
     </div>
   );
