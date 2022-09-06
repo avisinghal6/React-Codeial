31aec454025390d46f51cb425bc0ea17961274b6
Avi Singhal <avisinghal6@gmail.com>
2022-09-06 02:09:50 -0500

    toastify added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
+import { toast, ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastContainer
+      position={'top-right'}
+      autoClose={5000}
+      hideProgressBar={false}
+    />
+    {/* <toast autodismiss autodismissTimeout={5000} placement="top-left"> */}
     <App />
+
+    {/* </toast> */}
   </React.StrictMode>
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,45 @@
 import styles from '../styles/login.module.css';
-
+import { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// toast.configure();
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLogginIn] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLogginIn(true);
+
+    if (!email || !password) {
+      return toast('enter email and password');
+    }
+  };
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>Log In</span>
 
       <div className={styles.field}>
-        <input type="email" placeholder="Email" required />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
 
       <div className={styles.field}>
-        <input type="password" placeholder="Paasword" required />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
-      <div className={styles.field}>
-        <button>Log In</button>
+      <div className={styles.field} disabled={loggingIn}>
+        <button>{loggingIn ? 'Logging In..' : 'Log In'}</button>
       </div>
     </form>
   );
